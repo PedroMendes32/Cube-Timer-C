526bfc8f2437d967f1aca991ff633cfb34926b5d
Pedro Antonio Ciciliano Mendes <pedrocmendescoc2@gmail.com>
2022-11-09 13:29:17 -0300

    Adicionando as bibliotecas

---
+++ CuberTime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdbool.h>
+#include<locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL,"Portuguese");
+
+    printf("\n\n\n");
+    system("pause");
+
+    return 0;
+}
