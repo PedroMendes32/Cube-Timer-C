ecc8cc704f2db9f09ea51cca68c191c1a58c5bf4
Pedro Antonio Ciciliano Mendes <pedrocmendescoc2@gmail.com>
2022-11-09 14:42:13 -0300

    Criando a função para contar os tempos

--- CuberTime.c
+++ CuberTime.c
@@ -7,6 +7,9 @@
 void exibeMenu (void);
 void exibeAviso (void);
 void tempoExec (void);
+void cronometro (void);
+double conta (void);
+void salvar (void);
 
 int main(void)
 {
@@ -22,12 +25,7 @@ int main(void)
         switch (escolha)
         {
             case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
+                cronometro();
                 break;
             case 0:
                 status = !status;
@@ -48,14 +46,76 @@ void exibeMenu (void)
     printf("\t\t**********************\n");
     printf("\t\t*   CUBE TIMER C     *\n");
     printf("\t\t**********************\n");
-    printf(" 1 -> 2x2 \n");
-    printf(" 2 -> 3x3 \n");
-    printf(" 3 -> 4x4 \n");
-    printf(" 4 -> 5x5 \n");
+    printf(" 1 -> Iniciar cronômetro! \n");
     printf(" 0 -> Sair\n");
 }
 
 void exibeAviso (void) 
+{   
+    fflush(stdin);
+    printf(" PARA COMEÇAR PRESSIONE ENTER! ");
+    getchar();
+    printf("\n\n");
+}
+
+void tempoExec (void)
+{   
+    printf(" COMEÇOU A CONTAR!.. \n");
+    printf(" PRESSIONE ENTER PARA FECHAR!..");
+    while (true)
+    {
+        if ( getchar() )
+        {
+            break;
+        }
+    }
+    printf("\n\n\n");
+    printf(" TERMINOU! \n");
+}
+
+double conta (void)
+{   
+    double tempoF;
+    exibeAviso();
+    printf("\n");
+    size_t tempo;
+    tempo = clock();
+    tempoExec();
+    tempo = clock() - tempo;
+
+    tempoF = ((double)tempo/CLOCKS_PER_SEC);
+    
+    return tempoF;
+}
+
+void cronometro (void)
+{
+    int qtdSeries;
+    int i;
+
+    printf("\n Digite a quantidade de séries: ");
+    scanf("%d",&qtdSeries);
+
+    double * resultados;
+
+    resultados = (double*) malloc (sizeof(double) * qtdSeries);
+
+    for ( i = 1; i <= qtdSeries; i++ )
+    {
+        resultados[i] = conta();
+    }
+
+    system("pause");
+    printf(" TEMPOS OBTIDOS: \n");
+
+    for ( i = 1; i <= qtdSeries; i++ )
+    {
+        printf(" %d - %.2lf Segundos \n",i,resultados[i]);
+    }
+}
+
+void salvar (void)
 {
     return;
 }
+
